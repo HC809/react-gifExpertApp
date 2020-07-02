f9e861423d53d21fc41a1e2ecb6c56ed8cedb6eb
Hector Caballero <caballero.22.809@gmail.com>
2020-07-01 21:26:18 -0600

    docs agregado

---
+++ docs/precache-manifest.2cd7da98ce8643d9492081449fff8e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898c884a2307d6160fabf16b04ca2205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7752f273068918ffa08b",
+    "url": "/static/css/main.4ae41405.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7752f273068918ffa08b",
+    "url": "/static/js/main.80ab0d43.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
